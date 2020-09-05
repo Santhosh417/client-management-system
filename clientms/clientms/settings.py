101b2943af602b3d7787605bb0e0d76dcbe55599
Santhosh <santhosh.veeramalla@mutualofomaha.com>
2020-09-05 06:29:49 -0500

    Adding allowed hosts

--- clientms/clientms/settings.py
+++ clientms/clientms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'du#1l=+e2*f=(k0gpcg5xzy^od=fkyyp*jlwbw@6skq(j6iea4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
